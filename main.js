4e1d828f9ddffd37b40c2e382319b5756d757357
TC McCarthy <tc.mccarthy1@gmail.com>
2022-03-01 20:31:25 -0500

    The lights work!

--- main.js
+++ main.js
@@ -1,3 +1,15 @@
-/**
- * SILENCE IS GOLDEN
- */
+// Get the body
+const body = document.querySelector("body");
+
+//get the full list of lightswitches (in this case, 2)
+const lightswitches = document.querySelectorAll(".light-switch");
+
+// for each of the switches on the list, call a function, and refer to the current switch as `lightswitch`
+lightswitches.forEach(function (lightswitch) {
+  // for this lightswitch, add an event listener, that fires a function whenever we click on the switch
+  lightswitch.addEventListener("click", function (e) {
+    // e stores the click event, which allows us to stop any unwanted default behaviors (like if this were a button)
+    e.preventDefault(); // we prevent those behaviors
+    body.classList.toggle("light-off"); // we toggle .light-off on/off the body element
+  });
+});
